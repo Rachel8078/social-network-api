ef9b626a77dbe6fbdd38efcac35b373be25916f7
Rachel Marriott <69021829+Rachel8078@users.noreply.github.com>
2022-04-21 12:01:13 -0600

    test CRUD user routes

--- models/User.js
+++ models/User.js
@@ -1,8 +1,5 @@
 const { Schema, model } = require('mongoose');
 
-// create the User model using the UserSchema
-const User = model('User', UserSchema);
-
 const UserSchema = new Schema({
   username: {
     type: String,
@@ -35,5 +32,13 @@ const UserSchema = new Schema({
   }
 }
 )
+
+// Create a virtual called friendCount that retrieves the length of the user's friends array field on query.
+UserSchema.virtual('friendCount').get(function() {
+  return this.friends.reduce((total, friends) => total + friends.length + 1, 0);;
+});
+
+// create the User model using the UserSchema
+const User = model('User', UserSchema);
 
 module.exports = User;
--- routes/api/index.js
+++ routes/api/index.js
@@ -1,18 +1,7 @@
 const router = require('express').Router();
-// Import all of the API routes from /api/index.js (no need for index.js though since it's implied)
-const apiRoutes = require('./api');
-const htmlRoutes = require('./html/html-routes');
 const userRoutes = require('./user-routes');
 
 // add prefix of `/users` to routes created in `user-routes.js`
 router.use('/users', userRoutes);
-
-// add prefix of `/api` to all of the api routes imported from the `api` directory
-router.use('/api', apiRoutes);
-router.use('/', htmlRoutes);
-
-router.use((req, res) => {
-  res.status(404).send('<h1>😝 404 Error!</h1>');
-});
 
 module.exports = router;
